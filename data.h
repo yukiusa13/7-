708df9460bd887d0cbd6d1f138020db4ca6c104e
subaru13 <54984748+subaru13@users.noreply.github.com>
2020-01-25 17:43:23 +0900

    no message

---
+++ data.h
@@ -0,0 +1,32 @@
+#pragma once
+//当たり判定用の半径
+#define player_rad (20)
+#define enemy_rad  (37)
+#define shot_rad   (20)
+
+////shot関連////
+#define shotmax (32)
+#define missilemax (16)
+#define shotspeed (15)//弾の速度
+#define missilespeed (30)//ミサイルの速度
+#define shot_time (5)//通常弾の発射間隔
+#define missile_timer (60)//ミサイルの発射間隔
+
+////enemy関連
+#define  ENEMYMAX (32)
+
+
+
+//stageのスクロールスピード
+#define stage_speed (10)
+
+
+
+
+
+
+
+
+
+
+
